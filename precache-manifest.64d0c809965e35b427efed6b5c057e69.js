db87e9142118552b0923c043c3f6c5ec10b38fb8
nowfel2k <muhammednavfal1@gmail.com>
2020-09-27 20:34:47 +0530

    Updates

---
+++ precache-manifest.64d0c809965e35b427efed6b5c057e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b965e592bdae2f9277cdb6a84a7bc3ce",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "5141ab4660c904559f77",
+    "url": "/movie-search-app/static/css/main.40725e09.chunk.css"
+  },
+  {
+    "revision": "509e65ee57fe830e7516",
+    "url": "/movie-search-app/static/js/2.94c9b944.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search-app/static/js/2.94c9b944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5141ab4660c904559f77",
+    "url": "/movie-search-app/static/js/main.876f301f.chunk.js"
+  },
+  {
+    "revision": "7544d26a30c58e47ecab",
+    "url": "/movie-search-app/static/js/runtime-main.11b6daf0.js"
+  }
+]);
